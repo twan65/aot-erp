45734e2ca610f6ee39dfa7e8fef916189511dbd9
twan65 <cx.twan65@gmail.com>
2021-05-27 16:37:36 +0900

    sidebar分離作業

--- src/main/java/jp/co/aoterp/web/controller/MemberController.java
+++ src/main/java/jp/co/aoterp/web/controller/MemberController.java
@@ -1,14 +1,21 @@
 package jp.co.aoterp.web.controller;
 
 import jp.co.aoterp.constant.ViewNames;
+import jp.co.aoterp.web.api.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class MemberController {
 
+    private final MemberService memberService;
+
     @GetMapping("/m/search")
-    public String findAll() {
+    public String findAllMembers(Model model) {
+//        model.addAttribute("members", memberService.findAll());
         return ViewNames.MEMBER_SEARCH_PATH;
     }
 }
