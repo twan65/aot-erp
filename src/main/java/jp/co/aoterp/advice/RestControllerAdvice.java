95af0e4029d13d6ddfa0b70d707ef9964ec2732d
twan65 <cx.twan65@gmail.com>
2021-05-28 01:17:40 +0900

    バリデーションエラーハンドラの途中コミット

---
+++ src/main/java/jp/co/aoterp/advice/RestControllerAdvice.java
@@ -0,0 +1,52 @@
+package jp.co.aoterp.advice;
+
+import jp.co.aoterp.error.ApiError;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@org.springframework.web.bind.annotation.RestControllerAdvice
+public class RestControllerAdvice extends ResponseEntityExceptionHandler {
+//
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity<> handleException(Exception e) {
+//        log.error("エラー内容：" + e);
+//
+//        return new ResponseEntity<>(ErrorResponseDto.builder()
+//                .errorCode(MessageCode.E00001.getCode())
+//                .errorMessage(messageHelper.getMessage(MessageCode.E00001))
+//                .build(),
+//                HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.add(error.getField() + ": " + error.getDefaultMessage());
+        }
+        for (ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+            errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+        }
+
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+        return handleExceptionInternal(
+                ex, apiError, headers, apiError.getStatus(), request);
+    }
+
+}
---
+++ src/main/java/jp/co/aoterp/error/ApiError.java
@@ -0,0 +1,35 @@
+package jp.co.aoterp.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * status: the HTTP status code
+ * message: the error message associated with exception
+ * error: List of constructed error messages
+ */
+@Getter
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
+}
+
