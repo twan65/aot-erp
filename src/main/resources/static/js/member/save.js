7cda19cb731006098fb3216b8e139e10cfa6307d
twan65 <cx.twan65@gmail.com>
2021-05-28 00:33:05 +0900

    コミットされないエラー対応

---
+++ src/main/resources/static/js/member/save.js
@@ -0,0 +1,34 @@
+var save = {
+    init : function () {
+        var _this = this;
+
+        $('#btn-save').on('click', function () {
+            _this.save();
+        });
+    },
+    save : function () {
+        var data = {
+            email: $('#email-id').val(),
+            name: $('#name-id').val(),
+            department: $('#department-id').val(),
+            position: $('#position-id').val(),
+            age: $('#age-id').val(),
+            sex: $('#sex-id').val(),
+            nationality: $('#nationality-id').val(),
+        };
+        console.log(data);
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/m',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function(data) {
+            alert('アイデアが登録されました。');
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    }
+};
+
+save.init();
