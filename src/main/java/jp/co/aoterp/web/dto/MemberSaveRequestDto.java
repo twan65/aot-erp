55440d8954ddcd5bf54b8c92c782d76fda70c532
twan65 <cx.twan65@gmail.com>
2021-05-31 18:57:46 +0900

    メンバー登録画面は一旦終了

--- src/main/java/jp/co/aoterp/web/dto/MemberSaveRequestDto.java
+++ src/main/java/jp/co/aoterp/web/dto/MemberSaveRequestDto.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -14,15 +15,33 @@ import java.time.format.DateTimeFormatter;
 @NoArgsConstructor
 public class MemberSaveRequestDto {
 
-    @NotBlank(message = "{E00001}")
+    @NotBlank(message = "{E00002}")
+    @Email(message = "{E00004}")
     private String email;
+
+    @NotBlank(message = "{E00002}")
     private String name;
+
+    @Size(min = 1, max = 3, message = "{E00003}")
     private Long employmentType;
+
+    // TODO: Localdateに変更
+    @NotBlank(message = "{E00002}")
     private String enteredDate;
+
+    @Size(min = 1, max = 3, message = "{E00003}")
     private Long department;
+
+    @Size(min = 1, max = 5, message = "{E00003}")
     private Long position;
+
+    // TODO: 生年月日に変更
     private Long age;
+
+    @Size(min = 1, max = 3, message = "{E00003}")
     private Long sex;
+
+    @Size(min = 1, max = 5, message = "{E00003}")
     private Long nationality;
 
     @Builder
