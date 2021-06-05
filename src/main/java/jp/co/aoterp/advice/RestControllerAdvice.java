b1e363305b713cab3e5db51de36b43478be78b8c
twan65 <cx.twan65@gmail.com>
2021-06-06 01:56:00 +0900

    エラー画面表示はSpringbootのデフォルト・マッピングを利用

--- src/main/java/jp/co/aoterp/advice/RestControllerAdvice.java
+++ src/main/java/jp/co/aoterp/advice/RestControllerAdvice.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@org.springframework.web.bind.annotation.RestControllerAdvice(basePackages = "jp.co.aoterp.web.api")
+@org.springframework.web.bind.annotation.RestControllerAdvice
 public class RestControllerAdvice extends ResponseEntityExceptionHandler {
 
     @Override
