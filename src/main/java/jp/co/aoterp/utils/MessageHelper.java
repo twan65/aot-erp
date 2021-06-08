aafa0b5b19d3ebafc439efc2cc938c709adc7a9a
twan65 <cx.twan65@gmail.com>
2021-06-08 21:17:04 +0900

    add MessageHelper Util

---
+++ src/main/java/jp/co/aoterp/utils/MessageHelper.java
@@ -0,0 +1,27 @@
+package jp.co.aoterp.utils;
+
+import jp.co.aoterp.constant.MessageCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+@Component
+public class MessageHelper {
+
+  private final MessageSource messageSource;
+
+  public String getMessage(MessageCode messageCode) {
+    return this.getMessage(messageCode, null, Locale.getDefault());
+  }
+
+  public String getMessage(MessageCode messageCode, Object[] args) {
+    return this.getMessage(messageCode, args, Locale.getDefault());
+  }
+
+  public String getMessage(MessageCode messageCode, Object[] args, Locale locale) {
+    return messageSource.getMessage(messageCode.getCode(), args, locale);
+  }
+}
