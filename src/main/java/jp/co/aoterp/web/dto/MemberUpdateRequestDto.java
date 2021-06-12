f4addf2f6d0c81d7213168daba7486c040fd5d2e
twan65 <cx.twan65@gmail.com>
2021-06-12 20:00:36 +0900

    メンバー編集バリデーション追加。スキルはTODO

--- src/main/java/jp/co/aoterp/web/dto/MemberUpdateRequestDto.java
+++ src/main/java/jp/co/aoterp/web/dto/MemberUpdateRequestDto.java
@@ -3,16 +3,31 @@ package jp.co.aoterp.web.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Getter
 public class MemberUpdateRequestDto {
 
+    @NotBlank(message = "{E00002}")
     private String name;
+
+    @Min(value = 1, message = "{E00003}")
+    @Max(value = 3, message = "{E00003}")
     private Integer sex;
+
+    @Min(value = 1, message = "{E00003}")
+    @Max(value = 5, message = "{E00003}")
     private Integer nationality;
+
+    @NotBlank(message = "{E00002}")
     private String nearestStation;
+
+    @NotBlank(message = "{E00002}")
     private String phone;
+
     private List<SkillDto> skills;
 
     @Builder
