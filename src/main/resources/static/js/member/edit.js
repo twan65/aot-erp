e7823a991abc446de8a62533300ab66733c7d1a5
twan65 <cx.twan65@gmail.com>
2021-06-17 21:51:45 +0900

    スキル追加機能の追加

--- src/main/resources/static/js/member/edit.js
+++ src/main/resources/static/js/member/edit.js
@@ -5,6 +5,10 @@ var edit = {
         $('#btn-edit').on('click', function () {
             _this.edit();
         });
+
+        $('#add-skill').on('click', function () {
+            _this.addSkill();
+        });
     },
     edit : function () {
         var data = {
@@ -36,6 +40,38 @@ var edit = {
             $('#success-modal-body').text('メンバーが正常に更新されました。');
             $("#success").modal('show');
         });
+    },
+    addSkill : function () {
+        let length = $( "input[name^='skills']" ).length;
+        let targetIdx = length / 2;
+        $("#skill-body").append(`
+                            <div class='row'>
+                                <div class='col-12'>
+                                    <fieldset>
+                                        <div class='input-group'>
+                                            <div class='input-group-prepend'>
+                                                <span class='input-group-text'>スキル名</span>
+                                            </div>
+                                            <input type='text' aria-label='Skill name' class='form-control'
+                                                   name='skills[${targetIdx}].name' id='skills[${targetIdx}].name'>
+                                            <input type='text' aria-label='Skill percent' class='form-control'
+                                                   name='skills[${targetIdx}].percent' id='skills[${targetIdx}].percent'>
+                                            <div class='input-group-prepend'>
+                                                <span class='input-group-text'>パーセント(%)</span>
+                                            </div>
+
+                                        </div>
+                                    </fieldset>
+                                    <div id='skills[${targetIdx}].name-validation' class='invalid-feedback'>
+                                        <i class='bx bx-radio-circle'></i>
+                                    </div>
+                                    <div id='skills[${targetIdx}].percent-validation' class='invalid-feedback'>
+                                        <i class='bx bx-radio-circle'></i>
+                                    </div>
+                                </div>
+
+                            </div>
+        `);
     }
 };
 
